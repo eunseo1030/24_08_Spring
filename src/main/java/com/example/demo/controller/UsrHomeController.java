76359afd94085a677c7ebfd177eb88c8cc479dd0
eunseo1030 <eunseo20001030@gmail.com>
2024-08-09 15:26:06 +0900

    실행

---
+++ src/main/java/com/example/demo/controller/UsrHomeController.java
@@ -0,0 +1,18 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.example.demo.vo.Article;
+
+@Controller
+public class UsrHomeController {
+	@RequestMapping("/usr/home/getArticle")
+	@ResponseBody
+	public Article getArticle() {
+		Article article = new Article(1, "제목1", "내용1");
+		return article;
+	}
+	
+}
